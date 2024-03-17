4d4e687054b3db83bcc6be8475b4fa0b5e8ba703
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-03-17 00:33:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Jaunel Deans
+# September 14, 2023
+# We were using the print statement to test out different outcomes.
+name = "Jaunel Deans"
+print("My name is " +  name) 
+print("Summer was hot!")
+print("My favorite season is Fall because why not \n Also love to climb waterfalls-_-") 
+#\n breaks the sentence and creates a new line
+print("There are" , 12 , "packs of gum in my bag.")
+print("Moody Mondays")
